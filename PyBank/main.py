b88c83cd6b8ac34c78d0050a167aaecef8408384
braehpour <braehpour@gmail.com>
2020-08-09 13:50:24 -0500

    created variables to store total months, total profit/loss, and greatest increase/decrease in profit

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,5 +5,24 @@ file_path = os.path.join("Resources", "budget_data.csv")
 
 with open(file_path) as csvFile:
     csvReader = csv.reader(csvFile)
+    totalMonths = -1
+    totalProfitLoss = 0
+    currentIncrease = 0
+    currentDecrease = 0
+
     for row in csvReader:
-        print(row)
+        totalMonths = totalMonths + 1                           #store the value for total months
+    #print(totalMonths)
+
+        for col in csvReader:
+            totalProfitLoss += (int (col[1]))                   #store the value for total profit loss
+        #print(totalProfitLoss)
+
+            if (int (col[1])) > currentIncrease:
+                currentIncrease = (int (col[1]))                #store the greatest increase value
+        #print(currentIncrease)
+
+            if (int (col[1])) < currentDecrease:
+                currentDecrease = (int (col[1]))                #store the greatest decrease value
+        #print(currentDecrease)
+        
